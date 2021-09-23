95d5ede7d2e513e6f90a1d5ff242d28f5038274c
Zulfaqar Azmi <mzulfaqar88@gmail.com>
2021-09-24 07:35:30 +0900

    Change to class

--- src/imu_lpf.cpp
+++ src/imu_lpf.cpp
@@ -1,38 +1,48 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 
-ros::Subscriber imu_sub;
-ros::Publisher imu_lpf_pub;
-
 static double gravity[3] = {0, 0, 0};
 
-void imuLPF(const sensor_msgs::ImuConstPtr &msg)
-{
-  constexpr double alpha = 0.8;
-  sensor_msgs::Imu out_msg;
-  out_msg.header.stamp = ros::Time::now();
-  out_msg.header.frame_id = msg->header.frame_id;
+class ImuFiltersNode{
+public:
+  ImuFiltersNode()
+    :nh()
+  {
+    imu_lpf_pub = nh.advertise<sensor_msgs::Imu>("/imu/lpf/data", 5);
+    imu_sub = nh.subscribe("/imu/data", 5, &ImuFiltersNode::imuCallback, this);
+  }
 
-  gravity[0] = alpha * gravity[0] + ( 1 - alpha ) * msg->linear_acceleration.x;
-  gravity[1] = alpha * gravity[1] + ( 1 - alpha ) * msg->linear_acceleration.y;
-  gravity[2] = alpha * gravity[2] + ( 1 - alpha ) * msg->linear_acceleration.z;
+  ros::NodeHandle nh;
+  ros::Subscriber imu_sub;
+  ros::Publisher imu_lpf_pub;
+  void imuCallback(const sensor_msgs::ImuConstPtr &msg){
+    constexpr double alpha = 0.0;
+    low_pass_filter(msg, alpha);
+  }
 
-  out_msg.linear_acceleration.x = msg->linear_acceleration.x - gravity[0];
-  out_msg.linear_acceleration.y = msg->linear_acceleration.y - gravity[1];
-  out_msg.linear_acceleration.z = msg->linear_acceleration.z - gravity[2];
+  void low_pass_filter(const sensor_msgs::ImuConstPtr &msg, const double &alpha){
+    sensor_msgs::Imu out_msg;
+    out_msg.header.stamp = ros::Time::now();
+    out_msg.header.frame_id = msg->header.frame_id;
 
-  out_msg.angular_velocity = msg->angular_velocity;
+    gravity[0] = alpha * gravity[0] + (1 - alpha) * msg->linear_acceleration.x;
+    gravity[1] = alpha * gravity[1] + (1 - alpha) * msg->linear_acceleration.y;
+    gravity[2] = alpha * gravity[2] + (1 - alpha) * msg->linear_acceleration.z;
 
-  imu_lpf_pub.publish(out_msg);
-}
+    out_msg.linear_acceleration.x = msg->linear_acceleration.x - gravity[0];
+    out_msg.linear_acceleration.y = msg->linear_acceleration.y - gravity[1];
+    out_msg.linear_acceleration.z = msg->linear_acceleration.z - gravity[2];
+
+    out_msg.angular_velocity = msg->angular_velocity;
+
+    imu_lpf_pub.publish(out_msg);
+  }
+};
 
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "imu_lpf");
-  ros::NodeHandle nh;
-
-  imu_lpf_pub = nh.advertise<sensor_msgs::Imu>("/imu/lpf/data_raw", 5);
-  imu_sub = nh.subscribe("/imu/data", 5, imuLPF);
+  ImuFiltersNode node;
 
   ros::spin();
 }
